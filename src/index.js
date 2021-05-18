9e7890dd2c04391a427d6fd23eb092a6b327d4bc
anuragbhardwaj1303 <1914231.cse.coe@cgc.edu.in>
2021-05-18 19:14:52 +0530

    3.9

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDom from "react-dom";
 // import "./index.css";
-function Person() {
-  const btn = "search Button";
-  const first = "Anurag";
-  const last = "Bhardwaj";
+
+function People() {
   return (
     <section>
-      <button>{btn}</button>
-      <h2>{`Welcome Home ${first} ${last}`}</h2>
-      <p>info</p>
+      <Person name='Anurag' job='Web Dev.' age='19' />
+      <Person name='Deepti' job='Teacher' age='26' />
+      <Person name='Kirti' job='Doctor.' age='16' />
+      <Person name='Golu' job='Engineer' age='25' />
+      <Person name='Chiku' job='Chef' age='17' />
     </section>
   );
 }
+const Person = (props) => {
+  return (
+    <article>
+      <h3>Name : {props.name}</h3>
+      <h5>Job : {props.job}</h5>
+      <p>Age : {props.age}</p>
+      <hr></hr>
+    </article>
+  );
+};
 
-ReactDom.render(<Person />, document.getElementById("root"));
+ReactDom.render(<People />, document.getElementById("root"));
